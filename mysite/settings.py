ea17140870eb64a299aabaaf5915a36b4615bb60
B Anil Kumar <tech1arb@gmail.com>
2023-07-25 22:37:49 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 
     "default": {
         "ENGINE"  :  os.environ("DB_ENGINE"  , default="django.db.backends.sqlite3"),
-        "NAME"    :  os.environ("DB_NAME", default=os.path.join(BASE_DIR, "db.sqlite3")),
+        "NAME"    :  os.environ("DB_NAME", default='postgres'")),
         "USER"    :  os.environ("DB_USER"    , default=None),
         "PASSWORD":  os.environ("DB_PASSWORD", default=None),
         "HOST"    :  os.environ("DB_HOST"    , default=None),
